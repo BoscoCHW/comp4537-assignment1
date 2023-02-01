de04c7eae75d8c1e42784a8d00cf89b161c634f7
Bosco_Acer <bosco.chw@gmail.com>
2023-02-01 15:20:30 -0800

    update response json to pass tests

--- controllerV1.js
+++ controllerV1.js
@@ -4,52 +4,78 @@ const Pokemon = require("./models/Pokemon");
 
 router.get("/", (req, res) => res.sendStatus(200));
 
-router.get("/pokemons", (req, res) => {
+router.get("/pokemons", async (req, res) => {
   const { count, after } = req.query;
   const limit = count ? parseInt(count) : 10;
   const skip = after ? parseInt(after) : 0;
-  Pokemon.find({})
+  const pokemon = await Pokemon.find({})
+    .sort({ _id: 1 })
     .skip(skip)
     .limit(limit)
-    .then((pokemons) => {
-      res.json(pokemons);
-    })
-    .catch((err) => {
-      res.status(500).json(err);
-    });
+    .exec();
+  res.json(pokemon);
 });
-router.post("/pokemon", (req, res) => {
-  const { name, img, type, base } = req.body;
-  const newPokemon = new Pokemon({ name, img, type, base });
-  newPokemon
-    .save()
-    .then((pokemon) => {
+
+router.post("/pokemon", async (req, res) => {
+  const { id, name, img, type, base } = req.body;
+  try {
+    const newPokemon = await Pokemon.create({ _id: id, name, img, type, base });
+    res.json({msg: "Added Successfully", newPokemon});
+  } catch (err) {
+    if (err.code === 11000) {
+      res.status(400).json({ errMsg: "Pokemon already exists" });
+    } else {
+      res.status(500).json(err);
+    }
+  }
+});
+
+function parseId(id) {
+  try {
+    const intId = parseInt(id);
+    if (!intId || intId < 1 || intId > 811) {
+      throw new Error();
+    }
+    return intId;
+  } catch (err) {
+    throw new Error("Cast Error: pass pokemon id between 1 and 811");
+  }
+}
+
+router.get("/pokemon/:id", async (req, res) => {
+  let id;
+  try {
+    id = parseId(req.params.id);
+  } catch (err) {
+    res.status(400).json({ errMsg: err.message });
+    return;
+  }
+  try {
+    const pokemon = await Pokemon.findById(id).exec();
+    if (pokemon) {
       res.json(pokemon);
-    })
-    .catch((err) => {
-      res.status(500).json(err);
-    });
+    } else {
+      res.status(404).json({ errMsg: "Pokemon not found" });
+    }
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
-router.get("/pokemon/:id", (req, res) => {
+
+router.get("/pokemonImage/:id", async (req, res) => {
   const { id } = req.params;
-  Pokemon.findById(id)
-    .then((pokemon) => {
-      res.json(pokemon);
-    })
-    .catch((err) => {
-      res.status(500).json(err);
-    });
-});
-router.get("/pokemonImage/:id", (req, res) => {
-  const { id } = req.params;
-  Pokemon.findById(id)
-    .then((pokemon) => {
+  try {
+    const pokemon = await Pokemon.findById(parseInt(id)).exec();
+    if (pokemon) {
       res.json(pokemon.img);
-    })
-    .catch((err) => {
-      res.status(500).json(err);
-    });
-}); // - get a pokemon Image URL
+    } else {
+      res.status(404).json({ errMsg: "Pokemon not found" });
+    }
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
 router.put("/pokemon/:id", async (req, res) => {
   const { id } = req.params;
   const { name, img, type, base } = req.body;
@@ -63,12 +89,13 @@ router.put("/pokemon/:id", async (req, res) => {
       const updatedPokemon = await pokemon.save();
       res.json(updatedPokemon);
     } else {
-      res.status(404).json({ message: "Pokemon not found" });
+      res.status(404).json({ errMsg: "Pokemon not found" });
     }
   } catch (err) {
     res.status(500).json(err);
   }
 });
+
 router.patch("/pokemon/:id", async (req, res) => {
   const id = req.params.id;
   const updates = req.body;
@@ -78,7 +105,7 @@ router.patch("/pokemon/:id", async (req, res) => {
     }).exec();
     res.status(200).json(updatedUnicorn);
   } catch (err) {
-    res.status(400).send(err);
+    res.status(400).send({errMsg: err.message});
   }
 });
 
@@ -88,7 +115,7 @@ router.delete("/pokemon/:id", async (req, res) => {
   if (pokemon) {
     res.json(pokemon);
   } else {
-    res.status(404).json({ message: "Pokemon not found" });
+    res.status(404).json({ errMsg: "Pokemon not found" });
   }
 });
 
--- server.js
+++ server.js
@@ -10,6 +10,8 @@ const port = process.env.PORT || 3000;
 app.use(express.json());
 app.use("/api/v1", router);
 
+app.use("/*", (req, res) => res.status(404).json({ msg: "Improper route. Check API docs plz." }));
+
 app.listen(port, async () => {
   try {
     await mongoose.connect(
